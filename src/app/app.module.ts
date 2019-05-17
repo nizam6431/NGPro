acca04449aee519f7bfd2bf57df0444c248dc03f
nizam6431 <nizam6431@gmail.com>
2019-05-17 14:45:23 +0530

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NsTableComponent } from './ns-table/ns-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NsTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/ns-table/ns-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ns-table',
+  templateUrl: './ns-table.component.html',
+  styleUrls: ['./ns-table.component.css']
+})
+export class NsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
